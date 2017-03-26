1ce716d1af6e8c387e417e351ff50ef8ad524d10
Jack Lightbody <jack.lightbody@gmail.com>
2017-03-26 20:23:11 +0200

    Initial Commit

---
+++ Smart Wifi Toggler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Wifi Toggler
+//
+//  Created by Jack lightbody on 3/26/17.
+//  Copyright © 2017 Jack lightbody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
